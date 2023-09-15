688bec695e1be6cf6ee627754fffff1e30dea8bf
Alexander Dittner <alexander.dittner@gmail.com>
2023-09-15 02:08:45 +0200

    small changes

--- deploy.sh
+++ deploy.sh
@@ -17,17 +17,19 @@ echo
 
 echo "1/3: Create remote directories..."
 sshpass -P "$PWD_REQUEST_PHRASE" -p "$PWD" ssh -o StrictHostKeyChecking=no "$SERVER" "cd .. && 
-rm -r app/Mondmonolith/ && 
+rm -r app/Mondmonolith/api && 
+rm -r app/Mondmonolith/frontend && 
+rm -r app/Mondmonolith/nginx && 
 mkdir -p app/Mondmonolith/api/MondmonolithApi/target/ &&
 mkdir -p app/Mondmonolith/frontend/MondmonolithFrontend/"
 
+echo "2/3: Uploading docker files..."
 upload() {
 	echo "===> $1"
 	#use sshpass with flag -v for details
 	sshpass -P "$PWD_REQUEST_PHRASE" -p "$PWD" scp -r "$1" "$2"
 }
 
-echo "2/3: Uploading docker files..."
 upload api/Dockerfile "$SERVER":/app/Mondmonolith/api/
 upload frontend/Dockerfile "$SERVER":/app/Mondmonolith/frontend/
 upload frontend/nginx/ "$SERVER":/app/Mondmonolith/frontend/
